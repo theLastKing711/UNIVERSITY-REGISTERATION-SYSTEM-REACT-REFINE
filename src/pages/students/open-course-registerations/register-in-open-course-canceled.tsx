2c6c83438e74be29e66ddacd397d717c2f80e9c9
beshar jeiroudi <lastking711@protonmail.com>
2025-10-08 23:39:25 +0300

    a

--- src/pages/students/open-course-registerations/register-in-open-course-canceled.tsx
+++ src/pages/students/open-course-registerations/register-in-open-course-canceled.tsx
@@ -1,12 +1,33 @@
-import { useParsed } from "@refinedev/core";
+import { Link, useOne, useParsed } from "@refinedev/core";
 import React from "react";
+import {
+  STUDENT_OPEN_COURSE_REGISTERATION_REGISTERATION_CANCELED_URI,
+  STUDENT_OPEN_COURSE_REGISTERATION_URI,
+} from "../../../constants";
+import { Card, Typography } from "antd";
 
 const StudentOpenCourseRegisterationsRegisterationCanceled = () => {
   const { params } = useParsed();
 
-  console.log("params", params);
+  const { data } = useOne({
+    resource:
+      STUDENT_OPEN_COURSE_REGISTERATION_REGISTERATION_CANCELED_URI +
+      `?session_id=${params?.session_id}`,
+    id: -1,
+    queryOptions: {},
+  });
 
-  return <div>StudentOpenCourseRegisterationsRegisterationCanceled</div>;
+  return (
+    <Card title="فشلت عملية الدفع" style={{ marginTop: "1rem" }}>
+      <Typography style={{ marginBottom: "1rem" }}>
+        فشلت عملية التسجيل بالمادة
+        <span style={{ fontWeight: "bold" }}>{data?.data?.name}.</span>
+      </Typography>
+      <Link to={"/" + STUDENT_OPEN_COURSE_REGISTERATION_URI}>
+        العودة لتسجيل المواد
+      </Link>
+    </Card>
+  );
 };
 
 export default StudentOpenCourseRegisterationsRegisterationCanceled;
